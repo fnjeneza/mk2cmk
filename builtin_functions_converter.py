e31c3df40fe72778618a86bb3089fe237b6230af
Francois Njeneza <njenezamed@gmail.com>
2017-08-07 15:31:38 +0200

    define ifeq, ifneq and basename

--- builtin_functions_converter.py
+++ builtin_functions_converter.py
@@ -1,4 +1,14 @@
-def ifeq_(arg):
+def basename_():
+    """ return basename of path in args """
+    assert(isinstance(args, list))
+    basenames = []
+    for arg in args:
+        filename = os.path.basename(arg).split('.')[0]
+        basenames.append(os.path.join(os.path.dirname(arg), filename))
+
+    return basenames
+
+def ifeq_(arg1, arg2):
     """ convert ifeq expression
     ifeq (arg1, arg2)
     ifeq 'arg1', 'arg2'
@@ -7,7 +17,11 @@ def ifeq_(arg):
     ifeq 'arg1', "arg2"
     endif
     """
-    pass
+    return "if({} EQUAL {})".format(arg1, arg2)
+
+def ifneq_(arg1, arg2):
+    """Return ifneq cmake equivalent"""
+    return "if({} NOT EQUAL {})".format(arg1, arg2)
 
 def strip_(arg):
     """ convert strip function """
