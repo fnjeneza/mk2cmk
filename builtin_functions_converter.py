5c98f4cd5018054a70c16dc735f8dcf17af04cb1
Francois Njeneza <njenezamed@gmail.com>
2017-08-08 13:57:23 +0200

    add substitute method

--- builtin_functions_converter.py
+++ builtin_functions_converter.py
@@ -32,5 +32,27 @@ def ifdef_(arg):
 def wildcard_(arg):
     pass
 
-def subst_(arg):
-    pass
+def subst_(subst_expression):
+    """Return subst cmake equivalent"""
+    expr = """
+    set(output "")
+    string(REPLACE "{}" "{}" ${output} {})""".format(arg1, arg2, text)
+    return expr
+
+def _extract_subst_argument(expression):
+    """ extract substring argument from subst expression
+        $(subst _1,_2,_3)
+    """
+    space_position = expression.find(' ')
+    first_comma_position = expression.find(',')
+    second_comma_position = expression[first_comma_position+1:].find(',') +\
+                            first_comma_position
+    print(first_comma_position)
+    print(second_comma_position)
+
+    from_ = expression[space_position:first_comma_position]
+    to_ = expression[first_comma_position+1:second_comma_position+1]
+    length = len(expression)
+    text = expression[second_comma_position+2:length-1]
+
+    return (from_.strip(), to_.strip(), text.strip())
